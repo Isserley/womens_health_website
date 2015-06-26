33a9fa1b6a0c295ab5169837b725f2a90ee64ce0
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-06-26 17:02:02 -0600

    rspec testing: articles index, edit, new, and show

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
 
+  let(:article) {FactoryGirl.create(:article)}
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -18,16 +20,36 @@ RSpec.describe ArticlesController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get :edit, id: article.id
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: article.id
       expect(response).to have_http_status(:success)
     end
   end
+
+  # describe "PUT #update" do
+  #   it 'updates successfully' do
+  #     if @.update, 
+  #     expect
+
+  #   end
+  # end
+
+  # describe "DELETE #destroy" do
+  #   it 'deletes successfully' do
+
+  #   end
+  # end
+
+  # describe "POST #create" do
+  #   it 'creates successfully' do
+  
+  #   end
+  # end
 
 end
